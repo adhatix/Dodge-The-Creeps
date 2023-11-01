74afb7d79561830f5a6b7226f1faade3dc4d8354
Arish Khan <148265901+adhatix@users.noreply.github.com>
2023-11-01 14:37:21 -0400

    added player movement and collision

--- player.gd
+++ player.gd
@@ -1,7 +1,38 @@
 extends Area2D
+signal hit
 
-@export var speed = 400 # How fast the player will move in pixels per second
-var screen_size #size of game window/screen
+@export var speed = 400 #Measures the players speed
+var screen_size
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
+	
+func _process(delta):
+	var velocity = Vector2.ZERO #Players movement vector
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+func _on_body_entered(body):
+	pass # Replace with function body.
